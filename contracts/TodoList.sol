3c18e5ee4df8a1233ea5c67b42b9a1d06625ee75
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2023-11-08 17:29:31 +0200

    Create web application
    Signed-off-by: Brian Paidamoyo Sithole <brianacyth@icloud.com>

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,9 +3,23 @@ pragma solidity ^0.8.13;
 
 contract TodoList {
 
-  uint public taskCount;
+  struct Task {
+    uint id;
+    string title;
+    string description;
+    bool isCompleted;
+  }
+
+  uint public taskCount = 0;
+
+  mapping(uint => Task) public tasks;
 
   constructor() {
-    taskCount = 0;
+    createTask('Call SiGMA Inc.', 'Talk to Abigail/Sirine about blockchain development');
+  }
+
+  function createTask(string memory _title, string memory _description)  public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _title, _description, false);
   }
 }
